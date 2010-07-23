cbc783c9754eb2ed6b02e3b2105e217bec399920
Patrick Forhan <pforhan@muddyhorse.com>
2010-07-22 20:43:31 -0500

    initial commit

--- src/com/jadn/cc/services/FileSubscriptionHelper.java
+++ src/com/jadn/cc/services/FileSubscriptionHelper.java
@@ -1,21 +1,23 @@
 package com.jadn.cc.services;
 
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
 import android.util.Log;
 
+import com.jadn.cc.core.OrderingPreference;
+import com.jadn.cc.core.Subscription;
 import com.jadn.cc.core.Util;
 import com.jadn.cc.trace.TraceUtil;
 
 public class FileSubscriptionHelper implements SubscriptionHelper {
 
+    private static final String CONCAT_DIVIDER = "\\;";
+    private static final String REGEX_DIVIDER = "\\\\;";
     private final File legacyFile;
     private final File subscriptionFile;
 
@@ -25,41 +27,73 @@ public class FileSubscriptionHelper implements SubscriptionHelper {
     }
 
     @Override
-    public boolean addSubscription(String name, String url) {
-        Map<String, String> subs = getSubscriptions();
+    public boolean addSubscription(Subscription toAdd) {
+        List<Subscription> subs = getSubscriptions();
 
-        if (subs.containsKey(url)) {
+        if (containsSubscriptionURL(subs, toAdd.url)) {
             // we already have the URL stored:
             return false;
         } // endif
 
         // test the url:
-        if (Util.isValidURL(url)) {
+        if (Util.isValidURL(toAdd.url)) {
             // passed, put it in and save:
-            subs.put(url, name);
+            subs.add(toAdd);
             saveSubscriptions(subs);
 
             return true;
 
         } else {
-            Log.e("CarCast", "addSubscription: bad url: "+url);
+            Log.e("CarCast", "addSubscription: bad url: " + toAdd.url);
             return false;
         }
     }
 
+    /**
+     * Scan the list for a subscription by its URL
+     * 
+     * @param subs the list to scan
+     * @param url the URL to look for
+     * @return <code>true</code> if found in the list, <code>false</code>
+     *         otherwise.
+     */
+    private boolean containsSubscriptionURL(List<Subscription> subs, String url) {
+        return indexOfSubscriptionURL(subs, url) != -1;
+    }
+
+    /**
+     * Scan the list for a subscription by its URL
+     * 
+     * @param subs the list to scan
+     * @param url the URL to look for
+     * @return the index in the list, or -1 if not found
+     */
+    private int indexOfSubscriptionURL(List<Subscription> subs, String url) {
+        for (int i = 0; i < subs.size(); i++) {
+            Subscription sub = subs.get(i);
+            if (sub.url.equals(url)) {
+                return i;
+            } // endif
+        } // endfor
+
+        // not found:
+        return -1;
+    }
+
     @Override
-    public Map<String, String> getSubscriptions() {
+    public List<Subscription> getSubscriptions() {
         if (legacyFile.exists()) {
             // we need to convert to the new format first:
-            Map<String, String> legacy = getLegacySitesFromFile();
+            List<Subscription> legacy = getLegacySitesFromFile();
             saveSubscriptions(legacy);
             legacyFile.delete();
             // short-cut out:
             return legacy;
         }
+
         if (!subscriptionFile.exists()) {
             subscriptionFile.getParentFile().mkdirs();
-            resetToDemoSubscriptions();
+            return resetToDemoSubscriptions();
         }
         if (!subscriptionFile.exists()) {
             return null;
@@ -69,17 +103,17 @@ public class FileSubscriptionHelper implements SubscriptionHelper {
             Properties props = new Properties();
             props.load(dis);
 
-            return cleanupProperties(props);
+            return convertProperties(props);
 
         } catch (Exception e1) {
             TraceUtil.report(e1);
-            return Collections.emptyMap();
+            return Collections.emptyList();
         }
     }
 
-    Map<String,String> getLegacySitesFromFile() {
+    List<Subscription> getLegacySitesFromFile() {
         if (!legacyFile.exists()) {
-            return Collections.emptyMap();
+            return Collections.emptyList();
         }
         try {
             InputStream input = new FileInputStream(legacyFile);
@@ -87,12 +121,12 @@ public class FileSubscriptionHelper implements SubscriptionHelper {
 
         } catch (Exception e1) {
             TraceUtil.report(e1);
-            return Collections.emptyMap();
+            return Collections.emptyList();
         }
     }
 
-    Map<String, String> readLegacySites(InputStream input) throws IOException {
-        Map<String,String> sites = new HashMap<String, String>();
+    List<Subscription> readLegacySites(InputStream input) throws IOException {
+        List<Subscription> sites = new ArrayList<Subscription>();
         DataInputStream dis = new DataInputStream(input);
         String line = null;
         while ((line = dis.readLine()) != null) {
@@ -101,7 +135,7 @@ public class FileSubscriptionHelper implements SubscriptionHelper {
                 String name = line.substring(0, eq);
                 String url = line.substring(eq + 1);
                 if (Util.isValidURL(url)) {
-                    sites.put(url, name);
+                    sites.add(new Subscription(name, url));
                 } else {
                     TraceUtil.report(new RuntimeException("invalid URL in line: '" + line + "'; URL was: " + url));
                 } // endif
@@ -113,7 +147,7 @@ public class FileSubscriptionHelper implements SubscriptionHelper {
 
         return sites;
     }
-    
+
     /**
      * Insure that all properties are keyed properly, by URL. If a key is not a
      * url, but the value is, then the pair is removed, then placed back in
@@ -121,50 +155,73 @@ public class FileSubscriptionHelper implements SubscriptionHelper {
      * 
      * @param props the properties to scan
      */
-    Map<String, String> cleanupProperties(Properties props) {
-        Map<String, String> urlToName = new HashMap<String, String>();
+    List<Subscription> convertProperties(Properties props) {
+        List<Subscription> subscriptions = new ArrayList<Subscription>();
 
         Set<Object> keys = props.keySet();
         for (Object key : keys) {
-            String keyStr = (String) key;
-            if (Util.isValidURL(keyStr)) {
-                urlToName.put(keyStr, props.getProperty(keyStr));
-            } else {
-                // see if the value was a URL, instead:
-                String value = props.getProperty(keyStr);
-                if (Util.isValidURL(value)) {
-                    // if we are here, value worked but key didn't, so reverse
-                    // the props:
-                    urlToName.put(value, keyStr);
-                } else {
-                    // value wasn't a URL either, not much we can do here.
-                    Log.w("CarCast", "couldn't read subscription " + key + "=" + value);
-                } // endif
-
+            String url = (String) key;
+            String nameAndMore = props.getProperty(url, "");
+            Subscription sub = convertProperty(url, nameAndMore);
+            if (sub != null) {
+                subscriptions.add(sub);
             } // endif
         } // endforeach
 
-        return urlToName;
+        return subscriptions;
+    }
+
+    private Subscription convertProperty(String url, String nameAndMore) {
+        String[] split = nameAndMore.split(REGEX_DIVIDER);
+        
+        if (split.length == 3) {
+            // best case, we should have all properties:
+            try {
+                String name = split[0];
+                int maxCount = Integer.valueOf(split[1]);
+                OrderingPreference pref = OrderingPreference.valueOf(split[2]);
+                return new Subscription(name, url, maxCount, pref);
+
+            } catch (Exception ex) {
+                Log.w("CarCast", "couldn't read subscription " + url + "=" + nameAndMore);
+            } // endtry
+
+        } else if (split.length == 1) {
+            String name = split[0];
+            // oops, missing extra properties:
+            return new Subscription(name, url);
+
+        } else {
+            Log.w("CarCast", "couldn't read subscription " + url + "=" + nameAndMore);
+        } // endif
+        
+        return null;
     }
 
     @Override
-    public boolean removeSubscription(String url) {
-        Map<String, String> subs = getSubscriptions();
-        boolean wasPresent = subs.containsKey(url);
-        subs.remove(url);
-        saveSubscriptions(subs);
+    public boolean removeSubscription(Subscription toRemove) {
+        List<Subscription> subs = getSubscriptions();
+        int idx = indexOfSubscriptionURL(subs, toRemove.url);
+        if (idx != -1) {
+            subs.remove(idx);
+            saveSubscriptions(subs);
+            return true;
+        } // endif
 
-        return wasPresent;
+        return false;
     }
 
-    @Override
-    public boolean saveSubscriptions(Map<String, String> subscriptions) {
+    private boolean saveSubscriptions(List<Subscription> subscriptions) {
         try {
             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(subscriptionFile));
-            Properties subs = new Properties();
-            subs.putAll(subscriptions);
-
-            subs.store(bos, "Carcast Subscription File");
+            Properties outSubs = new Properties();
+            
+            for (Subscription sub : subscriptions) {
+                String valueStr = sub.name + CONCAT_DIVIDER + sub.maxDownloads + CONCAT_DIVIDER + sub.orderingPreference.name();
+                outSubs.put(sub.url, valueStr);
+            } // endforeach
+            
+            outSubs.store(bos, "Carcast Subscription File v2");
             bos.close();
 
             // success:
@@ -178,43 +235,37 @@ public class FileSubscriptionHelper implements SubscriptionHelper {
     }
 
     @Override
-    public void resetToDemoSubscriptions() {
-        Properties subs = new Properties();
-        subs.setProperty("http://www.discovery.com/radio/xml/sciencechannel.xml", "Science Channel");
-        subs.setProperty("http://www.cbc.ca/podcasting/includes/quirks.xml", "Quirks and Quarks");
-        subs.setProperty("http://www.cringely.com/feed/podcast/", "Cringely");
-        subs.setProperty("http://rss.sciam.com/sciam/60secsciencepodcast", "60 second science");
-        subs.setProperty("http://rss.sciam.com/sciam/60-second-psych", "60 second psych");
-        subs.setProperty("http://rss.sciam.com/sciam/60-second-earth", "60 second earth");
-        subs.setProperty("http://nytimes.com/services/xml/rss/nyt/podcasts/techtalk.xml", "New York Times Tech Talk");
-        Map<String, String> hack = Util.cast(subs);
-        saveSubscriptions(hack);
+    public List<Subscription> resetToDemoSubscriptions() {
+        List<Subscription> subs = new ArrayList<Subscription>();
+        subs.add(new Subscription("Science Channel", "http://www.discovery.com/radio/xml/sciencechannel.xml"));
+        subs.add(new Subscription("Quirks and Quarks", "http://www.cbc.ca/podcasting/includes/quirks.xml"));
+        subs.add(new Subscription("Cringely", "http://www.cringely.com/feed/podcast/"));
+        subs.add(new Subscription("60 second science", "http://rss.sciam.com/sciam/60secsciencepodcast"));
+        subs.add(new Subscription("60 second psych", "http://rss.sciam.com/sciam/60-second-psych"));
+        subs.add(new Subscription("60 second earth", "http://rss.sciam.com/sciam/60-second-earth"));
+        subs.add(new Subscription("New York Times Tech Talk",
+                "http://nytimes.com/services/xml/rss/nyt/podcasts/techtalk.xml"));
+        saveSubscriptions(subs);
+        return subs;
     }
 
-    public boolean editSubscription(String originalUrl, String name, String url) {
-        Map<String, String> subs = getSubscriptions();
-        try {
-            if (!subs.containsKey(originalUrl)) {
-                // we don't already have the URL stored:
-                return false;
-            } // endif
-
-            // test the url:
-            new URL(url);
-
-            // update the entry:
-            subs.remove(originalUrl);
-
-            subs.put(url, name);
+    @Override
+    public boolean editSubscription(Subscription original, Subscription updated) {
+        List<Subscription> subs = getSubscriptions();
+        int idx = indexOfSubscriptionURL(subs, original.url);
+        if (idx != -1) {
+            subs.remove(idx);
+            subs.add(updated);
             saveSubscriptions(subs);
-
             return true;
-
-        } catch (MalformedURLException e) {
-            Log.e("CarCast", "addSubscription", e);
-        }
+        } // endif
 
         return false;
     }
-
+    
+    @Override
+    public void deleteAllSubscriptions() {
+        List<Subscription> emptyList = Collections.emptyList();
+        saveSubscriptions(emptyList);
+    }
 }
--- src/com/jadn/cc/services/SubscriptionHelper.java
+++ src/com/jadn/cc/services/SubscriptionHelper.java
@@ -1,19 +1,14 @@
 package com.jadn.cc.services;
 
-import java.util.Map;
+import java.util.List;
+
+import com.jadn.cc.core.Subscription;
 
 public interface SubscriptionHelper {
-    public Map<String, String> getSubscriptions();
-    public boolean addSubscription(String name, String url);
-    public boolean removeSubscription(String url);
-    public boolean editSubscription(String originalUrl, String name, String url);
-
-    /**
-     * 
-     * @param subscriptions what to save
-     * @return <code>true</code> if save succeeded, <code>false</code> otherwise
-     */
-    public boolean saveSubscriptions(Map<String, String> subscriptions);
-
-    public void resetToDemoSubscriptions();
+    public List<Subscription> getSubscriptions();
+    public boolean addSubscription(Subscription toAdd);
+    public boolean removeSubscription(Subscription toRemove);
+    public boolean editSubscription(Subscription original, Subscription updated);
+    public List<Subscription> resetToDemoSubscriptions();
+    public void deleteAllSubscriptions();
 }
