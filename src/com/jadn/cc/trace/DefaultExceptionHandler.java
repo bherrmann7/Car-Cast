2c222cc5e8560f20a24ce417234663ceefd95f6c
Bob Herrmann <bob@jadn.com>
2011-03-19 23:01:13 -0400

    add logging statement

--- src/com/jadn/cc/trace/DefaultExceptionHandler.java
+++ src/com/jadn/cc/trace/DefaultExceptionHandler.java
@@ -3,6 +3,8 @@ package com.jadn.cc.trace;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 
+import android.util.Log;
+
 public class DefaultExceptionHandler implements UncaughtExceptionHandler {
 
 	private UncaughtExceptionHandler defaultExceptionHandler;
@@ -15,7 +17,8 @@ public class DefaultExceptionHandler implements UncaughtExceptionHandler {
 	// Default exception handler
 	public void uncaughtException(Thread thread, Throwable throwable) {		
 		TraceUtil.saveTrace(throwable);
-    	defaultExceptionHandler.uncaughtException(thread, throwable);        
+		Log.e("CarCast", "DefaultExceptionHandler unpleasantness", throwable);
+    	defaultExceptionHandler.uncaughtException(thread, throwable);
 	}
 	
 	
